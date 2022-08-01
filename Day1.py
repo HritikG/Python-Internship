0851cde385838571289bc163095e0c480c713ed0
HRITIK GARG <13hritik@gmail.com>
2022-08-01 23:52:09 +0530

    1,Aug

---
+++ Day1.py
@@ -0,0 +1,15 @@
+#Code By : Hritik Garg <4921852>
+#Taking Input From User
+#Converting it into Int data type.
+#Storing in variables x,y.
+
+x = int(input("Enter the Value of x :- "))
+y = int(input("Enter the Value of y :- "))
+
+#New variable declared named result which stores the equation.
+
+result = (x + y) *(x + y)
+
+#Printing the result variable to get the desired answer.
+
+print (result)
