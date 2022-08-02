f5458a25d627bda9c443c4bea7e62514dafcea65
HRITIK GARG <13hritik@gmail.com>
2022-08-02 23:51:47 +0530

    Final Day1

--- Day1.py
+++ Day1.py
@@ -11,8 +11,13 @@ y = int(input("Enter the Value of y :- "))
 
 #New variable declared named result which stores the equation.
 
+#Method1
 result = (x + y) *(x + y)
+
+#Method2
+result1 = (x+y) ** 2
 
 #Printing the result variable to get the desired answer.
 
 print (result)
+print (result1)
